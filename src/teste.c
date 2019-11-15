ca05dd67987a63063f7ec5f78d1f4b305f180fa0
Rodrigo <r.drigomachado@gmail.c.om>
2019-11-15 17:50:58 -0300

    Tudo toperson
    Leitura na MBR e Super Blocos está funcionando corretamente. A escrita nos Super Blocos está parcialmente pronta, falta criar checksum.

--- src/teste.c
+++ src/teste.c
@@ -5,54 +5,116 @@
 
 //0x7E32 em inteiro
 #define VERSAO 32306
-
-typedef struct particao{
-  int posicao_inicio;
-  int posicao_final;
-  int bitmap_inodes;
-  int bitmap_dados;
-  int area_inodes;
-  int area_dados;
-}Particao;
+int tamanho_setor;
 
 typedef struct superbloco{
   unsigned char id[4];
-  int versao;
-  int superblockSize;
-  int freeBlocksBitmapSize;
-  int freeInodeBitmapSize;
-  int inodeAreaSize;
-  int blockSize;
-  int diskSize;
+  int           versao;
+  int           superblockSize;
+  int           freeBlocksBitmapSize;
+  int           freeInodeBitmapSize;
+  int           inodeAreaSize;
+  int           blockSize;
+  int           diskSize;
   unsigned char Checksum[4];
   unsigned char reservado[232];
-}SuperBloco;
+} SuperBloco;
+
+typedef struct particao{
+  int posicao_inicio;
+  int posicao_fim;
+} Particao;
 
 Particao particoes[4];
 
-int main(){
+// DEBUG
+unsigned char           versao[2] ;
+unsigned char        tam_setor[2] ;
+unsigned char     byte_inicial[2] ;
+unsigned char  quant_particoes[2] ;
 
+int retornaSetorDoSuperBloco(int numero_particao){
+  return particoes[numero_particao].posicao_inicio;
+}
+
+int geraSuperBlocoESalva(int numero_particao, int setores_por_bloco){
+
+  int tamanhoSetor            = tamanho_setor;
+  int setoresPorBloco         = setores_por_bloco;
+  int blocosParaSuperBloco    = 1;
+  int bitsPorSetor            = tamanhoSetor * 8;
+  int bitsPorBloco            = setoresPorBloco * bitsPorSetor;
+
+  int tamanhoParticaoEmSetores  = particoes[numero_particao].posicao_fim - particoes[numero_particao].posicao_inicio;
+  int numeroBlocosNaParticao    = tamanhoParticaoEmSetores / setoresPorBloco;
+  // 10% para blocos de inode
+  int areaInodeEmBlocos         = (numeroBlocosNaParticao / 10);
+  if(numeroBlocosNaParticao % 10 > 0)
+    areaInodeEmBlocos++;
+
+  // numero de blocos necessarios para o bit map do inode
+  int blocosParaBitMapInode     = areaInodeEmBlocos / bitsPorBloco;
+  if (areaInodeEmBlocos % bitsPorBloco > 0){
+    blocosParaBitMapInode++;
+  }
+
+  // numero de blocos designados para área de dados
+  int areaDadosEmBlocos          = numeroBlocosNaParticao - (blocosParaSuperBloco + blocosParaBitMapInode + areaInodeEmBlocos);
+  // numero de blocos designados para área do bitmap de dados
+  int blocosParaBitMapDados      = areaDadosEmBlocos / bitsPorBloco;
+  if (areaDadosEmBlocos % bitsPorBloco > 0){
+    blocosParaBitMapDados++;
+  }
+  areaDadosEmBlocos                        = (int) areaDadosEmBlocos - blocosParaBitMapDados;
+
+  unsigned char* ponteiroAuxiliar;
+
+  unsigned char  bloco[256];
+  unsigned char  id[4];
+  int            versao           = 32306;
+  int            superblockSize   = 1;
+  int            blocosParticao   = (particoes[numero_particao].posicao_fim - particoes[numero_particao].posicao_inicio)/setoresPorBloco;
+
+  id[0] = 'T';id[1] = '2'; id[2] = 'F'; id[3] = 'S';
+  copiarMemoria((char*) bloco, (char*) id, 4);
+
+  //converteParaBigEndian((unsigned char*) &versao, 2);
+  ponteiroAuxiliar = (unsigned char*) &versao;
+  copiarMemoria((char*) &bloco[4], (char*) ponteiroAuxiliar, 2);
+  ponteiroAuxiliar = (unsigned char*) &superblockSize;
+  copiarMemoria((char*) &bloco[6], (char*) ponteiroAuxiliar, 2);
+  ponteiroAuxiliar = (unsigned char*) &blocosParaBitMapDados;
+  copiarMemoria((char*) &bloco[8], (char*) ponteiroAuxiliar, 2);
+  ponteiroAuxiliar = (unsigned char*) &blocosParaBitMapInode;
+  copiarMemoria((char*) &bloco[10], (char*) ponteiroAuxiliar, 2);
+  ponteiroAuxiliar = (unsigned char*) &areaInodeEmBlocos;
+  copiarMemoria((char*) &bloco[12], (char*) ponteiroAuxiliar, 2);
+  ponteiroAuxiliar = (unsigned char*) &setoresPorBloco;
+  copiarMemoria((char*) &bloco[14], (char*) ponteiroAuxiliar, 2);
+  ponteiroAuxiliar = (unsigned char*) &blocosParticao;
+  copiarMemoria((char*) &bloco[16], (char*) ponteiroAuxiliar, 4);
+  return write_sector (retornaSetorDoSuperBloco(numero_particao), bloco);
+}
+
+void leSetorEPreencheStructSuperBloco(SuperBloco *super_bloco, int numero_particao){
+
+  unsigned char  buffer_super_bloco[256];
+  int setor_do_super_bloco = retornaSetorDoSuperBloco(numero_particao);
+  read_sector(setor_do_super_bloco, buffer_super_bloco);
+
+  copiarMemoria((char*) &super_bloco->id, (char*) &buffer_super_bloco, 4);
+
+  super_bloco->versao                = converteDoisBytesParaInt((unsigned char*) &buffer_super_bloco[4]);
+  super_bloco->superblockSize        = converteDoisBytesParaInt((unsigned char*) &buffer_super_bloco[6]);
+  super_bloco->freeBlocksBitmapSize  = converteDoisBytesParaInt((unsigned char*) &buffer_super_bloco[8]);
+  super_bloco->freeInodeBitmapSize   = converteDoisBytesParaInt((unsigned char*) &buffer_super_bloco[10]);
+  super_bloco->inodeAreaSize         = converteDoisBytesParaInt((unsigned char*) &buffer_super_bloco[12]);
+  super_bloco->blockSize             = converteDoisBytesParaInt((unsigned char*) &buffer_super_bloco[14]);
+  super_bloco->diskSize              = converteDoisBytesParaInt((unsigned char*) &buffer_super_bloco[16]);
+}
+
+void le_MBR_Preenche_Dados_Particoes(){
   unsigned char         buffer[256] ;
-  unsigned char           versao[2] ;
-  unsigned char        tam_setor[2] ;
-  unsigned char     byte_inicial[2] ;
-  unsigned char  quant_particoes[2] ;
-
-  unsigned char     inicio_part0[4] ;
-  unsigned char        fim_part0[4] ;
-  unsigned char       nome_part0[24];
-
-  unsigned char     inicio_part1[4] ;
-  unsigned char        fim_part1[4] ;
-  unsigned char       nome_part1[24];
-
-  unsigned char     inicio_part2[4] ;
-  unsigned char        fim_part2[4] ;
-  unsigned char       nome_part2[24];
-
-  unsigned char     inicio_part3[4] ;
-  unsigned char        fim_part3[4] ;
-  unsigned char       nome_part3[24];
 
   read_sector(0, buffer);
 
@@ -60,178 +122,66 @@ int main(){
   copiarMemoria((char*) tam_setor,        (char*)  &buffer[2] , 2);
   copiarMemoria((char*) byte_inicial,     (char*)  &buffer[4] , 2);
   copiarMemoria((char*) quant_particoes,  (char*)  &buffer[6] , 2);
-
-  copiarMemoria((char*) inicio_part0,     (char*) &buffer[8] , 4);
-  copiarMemoria((char*) fim_part0,        (char*) &buffer[12], 4);
-  copiarMemoria((char*) nome_part0,       (char*) &buffer[16], 24);
-
-  copiarMemoria((char*) inicio_part1,     (char*) &buffer[40] , 4);
-  copiarMemoria((char*) fim_part1,        (char*) &buffer[44], 4);
-  copiarMemoria((char*) nome_part1,       (char*) &buffer[48], 24);
-
-  copiarMemoria((char*) inicio_part2,     (char*) &buffer[72] , 4);
-  copiarMemoria((char*) fim_part2,        (char*) &buffer[76], 4);
-  copiarMemoria((char*) nome_part2,       (char*) &buffer[80], 24);
-
-  copiarMemoria((char*) inicio_part3,     (char*) &buffer[104] , 4);
-  copiarMemoria((char*) fim_part3,        (char*) &buffer[108], 4);
-  copiarMemoria((char*) nome_part3,       (char*)  &buffer[112], 24);
-
   converteParaBigEndian(versao, 2);
-  converteParaBigEndian(tam_setor, 2);
   converteParaBigEndian(byte_inicial, 2);
   converteParaBigEndian(quant_particoes, 2);
 
-  converte(inicio_part0, 4);
-  converte(fim_part0, 4);
-  converte(inicio_part1, 4);
-  converte(fim_part1, 4);
-  converte(inicio_part2, 4);
-  converte(fim_part2, 4);
-  converte(inicio_part3, 4);
-  converte(fim_part3, 4);
-
-
-
-  particoes[0].posicao_inicio = binToDec(inicio_part0);
-  particoes[0].posicao_final  = binToDec(fim_part0);
-
-  particoes[1].posicao_inicio = binToDec(inicio_part1);
-  particoes[1].posicao_final  = binToDec(fim_part1);
-
-  particoes[2].posicao_inicio = binToDec(inicio_part2);
-  particoes[2].posicao_final  = binToDec(fim_part2);
-
-  particoes[3].posicao_inicio = binToDec(inicio_part3);
-  particoes[3].posicao_final  = binToDec(fim_part3);
+  tamanho_setor = converteDoisBytesParaInt(tam_setor);
 
   int index;
-  int setoresPorBloco         = 4;
-  int blocosParaSuperBloco    = 1;
-  int bitsPorSetor            = 256 * 8;
-  int bitsPorBloco            = setoresPorBloco * bitsPorSetor;
-
-
-
-
-
   for (index = 0; index < 4; index++){
+    Particao particao;
+    copiarMemoria((char*) &particao.posicao_inicio,    (char*) &buffer[8 + (index * 32)] , 4);
+    copiarMemoria((char*) &particao.posicao_fim,       (char*) &buffer[12 + (index * 32)], 4);
+    particoes[index] = particao;
+  }
+}
 
-    int tamanhoParticaoEmSetores  = particoes[index].posicao_final - particoes[index].posicao_inicio;
-
-    int numeroBlocosNaParticao    = tamanhoParticaoEmSetores / setoresPorBloco;
-
-    // 10% para blocos de inode
-    int areaInodeEmBlocos         = (int) (numeroBlocosNaParticao * 0.1) + ((numeroBlocosNaParticao % 10) > 0? 1: 0);
-
-    // numero de blocos necessarios para o bit map do inode
-    int blocosParaBitMapInode     = areaInodeEmBlocos / bitsPorBloco;
-    if (areaInodeEmBlocos % bitsPorBloco > 0){
-      blocosParaBitMapInode++;
-    }
-
-    // numero de blocos designados para área de dados
-    int areaDadosEmBlocos          = numeroBlocosNaParticao - (blocosParaSuperBloco + blocosParaBitMapInode + areaInodeEmBlocos);
-
-    // numero de blocos designados para área do bitmap de dados
-    int blocosParaBitMapDados      = areaDadosEmBlocos / bitsPorBloco;
-    if (areaDadosEmBlocos % bitsPorBloco > 0){
-      blocosParaBitMapDados++;
-    }
-
-    areaDadosEmBlocos              = (int) areaDadosEmBlocos - blocosParaBitMapDados;
-
-    particoes[index].bitmap_inodes =  blocosParaBitMapInode;
-    particoes[index].bitmap_dados  =  blocosParaBitMapDados;
-    particoes[index].area_inodes   =  areaInodeEmBlocos;
-    particoes[index].area_dados    =  areaDadosEmBlocos;
-
- }
-
+void printDadosMBReParticoes(){
   printf("------------------------------------------------------------\n");
   printf("Versao: ");          printHexa(versao, 2);          printf("\n");
   printf("Tamanho: ");         printHexa(tam_setor, 2);       printf("\n");
   printf("Byte inicial: ");    printHexa(byte_inicial, 2);    printf("\n");
   printf("Quantidade part: "); printHexa(quant_particoes, 2); printf("\n");
 
+  int index;
   for (index = 0; index < 4; index++){
     printf("---------------------------------------------------------------\n");
     printf("--Particao %d--\n", index);
     printf("Inicio       : %d\n", particoes[index].posicao_inicio);
-    printf("Fim          : %d\n", particoes[index].posicao_final );
-    printf("Blocos Inode : %d\n", particoes[index].area_inodes   );
-    printf("Blocos Dados : %d\n", particoes[index].area_dados    );
-    printf("Bitmap Inodes: %d\n", particoes[index].bitmap_inodes );
-    printf("Bitmap Dados : %d\n", particoes[index].bitmap_dados  );
-
-
-    //Preencher SuperBloco------------------------------------------------------
-    unsigned char  bloco[256];
-    unsigned char  id[4];
-    unsigned char* ponteiroAuxiliar;
-    int            versao           = 32306;
-    int            superblockSize   = 1;
-    int            blocosParticao   = (particoes[index].posicao_final - particoes[index].posicao_inicio)/setoresPorBloco;
-
-    id[0] = 'T';id[1] = '2'; id[2] = 'F'; id[3] = 'S';
-    copiarMemoria((char*) bloco, (char*) id, 4);
-
-    //converteParaBigEndian((unsigned char*) &versao, 2);
-    ponteiroAuxiliar = (unsigned char*) &versao;
-    copiarMemoria((char*) &bloco[4], (char*) ponteiroAuxiliar, 2);
-
-    ponteiroAuxiliar = (unsigned char*) &superblockSize;
-    copiarMemoria((char*) &bloco[6], (char*) ponteiroAuxiliar, 2);
-
-    ponteiroAuxiliar = (unsigned char*) &particoes[index].bitmap_dados;
-    copiarMemoria((char*) &bloco[8], (char*) ponteiroAuxiliar, 2);
-
-    ponteiroAuxiliar = (unsigned char*) &particoes[index].bitmap_inodes;
-    copiarMemoria((char*) &bloco[10], (char*) ponteiroAuxiliar, 2);
-
-    ponteiroAuxiliar = (unsigned char*) &particoes[index].area_inodes;
-    copiarMemoria((char*) &bloco[12], (char*) ponteiroAuxiliar, 2);
-
-    ponteiroAuxiliar = (unsigned char*) &setoresPorBloco;
-    copiarMemoria((char*) &bloco[14], (char*) ponteiroAuxiliar, 2);
-
-    ponteiroAuxiliar = (unsigned char*) &blocosParticao;
-    copiarMemoria((char*) &bloco[16], (char*) ponteiroAuxiliar, 4);
-
-
-    // recuperar e printar superbloco
-    printf("\n--SuperBloco--\n");
-    SuperBloco superbloco;
-    copiarMemoria((char*) &(superbloco.id), (char*) &bloco, 4);
-    printf("ID: ");
-    int x;
-    for(x=0; x<4; x++){
-      printf("%c", superbloco.id[x]);
-    }
-
-    superbloco.versao               = converteDoisBytesParaInt((unsigned char*) &bloco[4]);
-    printf("\nVersao: %X\n", superbloco.versao);
-
-    superbloco.superblockSize       = converteDoisBytesParaInt((unsigned char*) &bloco[6]);
-    printf("Tamanho SuperBloco: %d\n", superbloco.superblockSize);
-
-    superbloco.freeBlocksBitmapSize = converteDoisBytesParaInt((unsigned char*) &bloco[8]);
-    printf("Tamanho Bitmap Dados: %d\n", superbloco.freeBlocksBitmapSize);
-
-    superbloco.freeInodeBitmapSize  = converteDoisBytesParaInt((unsigned char*) &bloco[10]);
-    printf("Tamanho Bitmap Inodes: %d\n", superbloco.freeInodeBitmapSize);
-
-    superbloco.inodeAreaSize        = converteDoisBytesParaInt((unsigned char*) &bloco[12]);
-    printf("Tamanho Area Inodes: %d\n", superbloco.inodeAreaSize);
-
-    superbloco.blockSize            = converteDoisBytesParaInt((unsigned char*) &bloco[14]);
-    printf("Tamanho do Bloco: %d\n", superbloco.blockSize);
-
-    superbloco.diskSize = converteDoisBytesParaInt((unsigned char*) &bloco[16]);
-    printf("Tamanho Disco em Blocos: %d\n", superbloco.diskSize);
-
-
+    printf("Fim          : %d\n", particoes[index].posicao_fim );
   }
+}
 
+void printSuperBloco(SuperBloco super_bloco){
+    printf("---------------------------------------------------------------\n");
+    printf("--Super Bloco--\n");
+    printf("Id: ");
+    int index;
+    for (index=0; index<4;index++)
+      printf("%c", super_bloco.id[index]);
+
+    printf("\n");
+    printf("Versao: %X\n", super_bloco.versao);
+    printf("SuperBlock Size: %d\n", super_bloco.superblockSize);
+    printf("freeBlocksBitmapSize: %d\n", super_bloco.freeBlocksBitmapSize);
+    printf("freeInodeBitmapSize: %d\n", super_bloco.freeInodeBitmapSize);
+    printf("inodeAreaSize: %d\n", super_bloco.inodeAreaSize);
+    printf("blockSize: %d\n", super_bloco.blockSize);
+    printf("diskSize: %d\n", super_bloco.diskSize);
+}
+
+SuperBloco super_bloco_atual;
+
+int main(){
+  le_MBR_Preenche_Dados_Particoes();
+  printDadosMBReParticoes();
+
+  int index;
+  for(index = 0; index<4; index++){
+    geraSuperBlocoESalva(index, 4);
+    leSetorEPreencheStructSuperBloco(&super_bloco_atual, index);
+    printSuperBloco(super_bloco_atual);
+  }
   return 0;
 }
